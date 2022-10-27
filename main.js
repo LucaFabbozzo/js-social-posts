535a0da06de956acdcb7fc5b0f30f4a151632360
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-27 07:39:14 -0500

    [add] dynamic print posts

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
 // Al click su un pulsante “Mi Piace” di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 
 
-
+//Base Dati
 
 const posts = [
     {
@@ -71,3 +71,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+//Prendo il mio container
+
+const container = document.querySelector('#container');
+
+//Stampo i miei post dinamicamente
+
+posts.forEach(element => {
+    const post = `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">Phil Mangione</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__image">
+        <img src="https://unsplash.it/600/300?image=171" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div> 
+    </div>            
+    </div>
+    `;
+
+    container.innerHTML += post;
+})
